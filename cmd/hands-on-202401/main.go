35e292c430b9261d4fe2d5f40684c71bfa623ca5
Senoue <hiro@senoue.net>
2024-01-10 10:28:55 +0900

    Echo フレームワーク入門 ルーティングとミドルウェア
    Signed-off-by: Senoue <hiro@senoue.net>

--- cmd/hands-on-202401/main.go
+++ cmd/hands-on-202401/main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
 	"net/http"
 
+	"github.com/labstack/echo/v4"
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -20,19 +20,61 @@ func main() {
 	}
 	defer db.Close()
 
-	// ハンドラの登録
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		slice := make([]string, 0, 10)
-		for _, v := range []string{"a", "b", "c"} {
-			slice = append(slice, fmt.Sprintf("%d", v))
-		}
-		fmt.Fprint(w, "Hello, World!")
+	// // ハンドラの登録
+	// http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+	// 	slice := make([]string, 0, 10)
+	// 	for _, v := range []string{"a", "b", "c"} {
+	// 		slice = append(slice, fmt.Sprintf("%d", v))
+	// 	}
+	// 	fmt.Fprint(w, "Hello, World!")
+	// })
+
+	// // サーバーの起動
+	// log.Println("start http server :80")
+	// err = http.ListenAndServe(":80", nil)
+	// if err != nil {
+	// 	log.Fatalf("failed to serve: %v", err)
+	// }
+
+	// Echo による実装
+	e := echo.New()
+	e.GET("/hello", func(c echo.Context) error {
+		// レスポンスの返却
+		// return c.String(http.StatusOK, "Hello, World!")
+
+		// JSON を返す場合
+		return c.JSON(http.StatusOK, map[string]string{
+			"message": "Hello, World!",
+		})
 	})
 
+	e.POST("/hello", func(c echo.Context) error {
+		// リクエストボディの取得
+
+		// curl -X POST -H "Content-Type: application/json" -d '{"name":"foo"}' http://localhost:80/hello
+		req := new(struct {
+			Name string `json:"name"`
+		})
+		if err := c.Bind(req); err != nil {
+			return err
+		}
+		// レスポンスの返却
+		return c.JSON(http.StatusOK, map[string]string{
+			"message": "Hello, " + req.Name + "!",
+		})
+	})
+
+	// ミドルウェアの登録
+	e.Use(middleware.BasicAuth(func(username, password string, c echo.Context) (bool, error) {
+		if subtle.ConstantTimeCompare([]byte(username), []byte("sendai")) == 1 &&
+			subtle.ConstantTimeCompare([]byte(password), []byte("go")) == 1 {
+			// 認証成功
+			return true, nil
+		}
+		// 認証失敗時はfalseを返す
+		return false, nil
+	}))
+
 	// サーバーの起動
-	log.Println("start http server :80")
-	err = http.ListenAndServe(":80", nil)
-	if err != nil {
-		log.Fatalf("failed to serve: %v", err)
-	}
+	e.Logger.Fatal(e.Start(":80"))
 }
